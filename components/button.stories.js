c2cae557c33f3b4a3bfdbd048b963d885f58b667
neuefische <neuefische@neuefisches-MacBook-Air.fritz.box>
2022-03-10 15:10:06 +0100

    last updates from 8.

---
+++ components/button.stories.js
@@ -0,0 +1,7 @@
+import "./button.css";
+export default {
+  title: "components-Button",
+};
+export const Default = () => '<button class="button">Save</button>';
+export const Large = () =>
+  '<button class="button button--large  ">Discard</button>';
---
+++ components/header.stories.js
@@ -0,0 +1,8 @@
+import "./header.css";
+export default {
+  title: "components-Header",
+};
+export const h1 = () => "<h1>Hello World<h1>";
+export const h2 = () => "<h2>Hello World<h2>";
+export const h3 = () => "<h3>Hello World<h3>";
+export const h4 = () => "<h4>Hello World<h4>";
